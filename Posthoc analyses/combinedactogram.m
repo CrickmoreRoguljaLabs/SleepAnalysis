c0185b04117f454a02297b1ace05efd5537b3f61
Stephen Zhang <x.stephen.zhang@gmail.com>
2016-08-24 11:05:52 -0400

    updated

--- Posthoc analyses/combinedactogram.m
+++ Posthoc analyses/combinedactogram.m
@@ -8,7 +8,7 @@ day_end = 20;
 binsize_raw = 1;
 
 % Define bin-size (in min)
-binsize = 30;
+binsize = 15;
 n_bins = 1440 / binsize;
 
 % Default path
@@ -27,7 +27,7 @@ night = 13:36;
 % Figure sizes [width, height])
 figsize = [600 300];
 
-% Color of the night (0=black, 1=white)
+% Color of the night (0 = black, 1 = white)
 nightcolor = 0.4;
 
 % Max y-axis
@@ -36,10 +36,14 @@ Y_max = 60;
 % Anticipation window (in hours)
 awindow = 3;
 
+% Set the size of the error
+errorsize = 12;
+
 %% Preprocessing
 
 % Generate folder directory
-folderls = ls(fullfile(filepath , '*.mat'));
+folderls = dir(fullfile(filepath , '*.mat'));
+folderls = {folderls.name};
 
 % Obtain the number of files
 n_files = size(folderls , 1);
@@ -68,10 +72,10 @@ geno_num_chosen = zeros(n_files,1);
 % Go through the .mat files in the folder to make sure the genotype can be
 % found in all of them
 for i = 1 : n_files
-    disp(['Checking ', folderls(i,:), '...'])
+    disp(['Checking ', folderls{i}, '...'])
     
     % Load new genotypes
-    load(fullfile(filepath, folderls(i,:)), 'genos', 'n_genos');
+    load(fullfile(filepath, folderls{i}), 'genos', 'n_genos');
     
     % Find the genotype
     geno_lookup = strcmp(geno_chosen, genos);
@@ -90,9 +94,15 @@ for i = 1 : n_files
         disp([num2cell(1:n_genos)', genos])
         
         % Let user choose genotype
-        genonum = input('Which genotype to choose (use number) = ');
-        geno_chosen_cell{i} = genos{genonum};
-        geno_num_chosen(i) = genonum;
+        genonum = input('Which genotype to choose (use number; 0 = none) = ');
+        
+        if genonum ~= 0
+            geno_chosen_cell{i} = genos{genonum};
+            geno_num_chosen(i) = genonum;
+        else
+            geno_chosen_cell{i} = '';
+            geno_num_chosen(i) = 0;
+        end
     end
 end
 
@@ -103,30 +113,38 @@ data_mat = [];
 
 for i = 1 : n_files
     % Initiate processing
-    disp(['Processing ', folderls(i,:), '...'])
+    disp(['Processing ', folderls{i}, '...'])
     
-    % Load the master data structure
-    load(fullfile(filepath, folderls(i,:)),...
-        'master_data_struct', 'n_sleep_bounds');
-    
-    % Extract unbinned activity data
-    unbinned_data_temp = master_data_struct(i).data;
-    
-    % Reshape unbinned activity data
-    unbinned_data_reshapen = reshape(unbinned_data_temp, ...
-        [binsize/binsize_raw, 1440/(binsize/binsize_raw), ...
-        n_sleep_bounds/2, size(unbinned_data_temp,2)]);
-    
-    % Bin the data
-    binned_data = sum(unbinned_data_reshapen, 1);
-    binned_data = mean(binned_data, 3);
-    binned_data = squeeze(binned_data)';
-    
-    % Display the number of flies added
-    disp(['Adding ', num2str(size(unbinned_data_temp,2)), ' flies.'])
-    
-    % Store the binned_data
-    data_mat = [data_mat; binned_data]; %#ok<AGROW>
+    if geno_num_chosen(i) ~= 0
+        % Load the master data structure
+        load(fullfile(filepath, folderls{i}),...
+            'master_data_struct', 'n_sleep_bounds');
+
+        % Extract unbinned activity data
+        unbinned_data_temp = master_data_struct(geno_num_chosen(i)).data;
+
+        % Remove deadflies
+        unbinned_data_temp = unbinned_data_temp(:,...
+            master_data_struct(geno_num_chosen(i)).alive_fly_indices>0);
+
+        % Reshape unbinned activity data
+        unbinned_data_reshapen = reshape(unbinned_data_temp, ...
+            [binsize/binsize_raw, 1440/(binsize/binsize_raw), ...
+            n_sleep_bounds/2, size(unbinned_data_temp,2)]);
+
+        % Bin the data
+        binned_data = sum(unbinned_data_reshapen, 1);
+        binned_data = mean(binned_data, 3);
+        binned_data = squeeze(binned_data)';
+
+        % Display the number of flies added
+        disp(['Adding ', num2str(size(unbinned_data_temp,2)), ' flies.'])
+
+        % Store the binned_data
+        data_mat = [data_mat; binned_data]; %#ok<AGROW>
+    else
+        disp('Skipping..')
+    end
 end
 
 %% Generate the bar graph
@@ -148,7 +166,7 @@ day = (n_bins/4 + 1 - (8-day_start)*(60/binsize)) :...
     ((day_end - 20) * 2 + n_bins * 0.75);
 
 % Make bar graphs for day and night
-figure('Position',[50,50,figsize(1),figsize(2)]);
+hfig = figure('Position',[50,50,figsize(1),figsize(2)]);
 
 hold on
 % Bars
@@ -157,9 +175,9 @@ bar(night, act_mean2(night), 1,'FaceColor',[nightcolor nightcolor nightcolor]);
 
 % Errors
 scatter(day,act_mean2(day)+act_sem2(day),...
-    'MarkerEdgeColor',[0 0 0], 'SizeData', 12);
+    'MarkerEdgeColor',[0 0 0], 'SizeData', errorsize);
 scatter(night,act_mean2(night)+act_sem2(night),...
-'MarkerEdgeColor',[0 0 0], 'SizeData', 12,...
+'MarkerEdgeColor',[0 0 0], 'SizeData', errorsize,...
     'MarkerFaceColor',[nightcolor nightcolor nightcolor]);
 hold off
 
@@ -180,6 +198,9 @@ ylim([0 Y_max])
 % Set figure font size
 set(gca,'FontSize',9)
 
+% Save figure
+print(fullfile([filepath,geno_chosen(1:10),'.pdf']),'-dpdf')
+
 %% Calculate anticipation
 % Rearrange data matrix to center on 8-8
 data_mat2 = data_mat(: ,...
@@ -192,13 +213,20 @@ anticipationmat_m = data_mat2(:,...
 
 % Evening matrix
 anticipationmat_e = data_mat2(:,...
-    (max(day) - awindow*(60/binsize) - 1) : (max(day)));
+    (max(day) - awindow*(60/binsize) + 1) : (max(day)));
 
 % Summed data
 % Morning sum
-disp(['Morning anticipation in ', num2str(awindow),'-hour window'])
-anticipation_m = sum(anticipationmat_m,2)
+disp(['Calculating morning anticipation in ',...
+    num2str(awindow),'-hour window'])
+anticipation_m = sum(anticipationmat_m,2);
 
 % Evening sum
-disp(['Evening anticipation in ', num2str(awindow),'-hour window'])
-anticipation_e = sum(anticipationmat_e,2)
+disp(['Calculating evening anticipation in ',...
+    num2str(awindow),'-hour window'])
+anticipation_e = sum(anticipationmat_e,2);
+
+% Write anticipations
+% First column is morning anticipation; Second is evening
+cell2csv(fullfile([filepath,geno_chosen,'_anticipation.csv']),...
+num2cell([anticipation_m,anticipation_e]));
